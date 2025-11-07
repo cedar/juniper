331480905e587b12452bd3a4963c9726e2d7cbac
sehrispk <stephan.sehring@gmail.com>
2025-11-07 08:57:47 +0100

    update

--- demos/example_coordinate_transform.py
+++ demos/example_coordinate_transform.py
@@ -16,19 +16,19 @@ M_base_2_cam = M_base_2_cam.at[:,3].set(v_base_to_cam)
 def base_to_cam_joint(joint_angles):
     return M_base_2_cam
 
-v_joint_to_cam = jnp.array([0,0,0.0,1]) #jnp.array([0,0,0.097,1])
+v_joint_to_cam = jnp.array([0,0,0.197,1])
 def cam_base_to_joint(joint_angles):
-    tilt = joint_angles[0][0]
-    pan = joint_angles[1][0]
+    tilt = joint_angles[1][0] / 180 * jnp.pi *0
+    pan = -joint_angles[0][0] / 180 * jnp.pi *0
 
-    #pan_mat = jnp.array([[1,0,0,0], [0, jnp.cos(pan), -jnp.sin(pan), 0], [0, jnp.sin(pan), jnp.cos(pan), 0], [0,0,0,1]])
+    roll_mat = jnp.eye(4)
     tilt_mat = jnp.array([[jnp.cos(tilt),0,jnp.sin(tilt),0], [0, 1, 0, 0], [-jnp.sin(tilt), 0, jnp.cos(tilt), 0], [0,0,0,1]])
     pan_mat = jnp.array([[jnp.cos(pan),-jnp.sin(pan),0,0], [jnp.sin(pan), jnp.cos(pan), 0, 0], [0, 0, 1, 0], [0,0,0,1]])
-    Rot_mat = pan_mat @ tilt_mat
-    Rot_mat = Rot_mat.T
+    Rot_mat = pan_mat @ tilt_mat @ roll_mat
+    #Rot_mat = Rot_mat.T
     
     Rot_mat = Rot_mat.at[:,3].set(v_joint_to_cam)
-    #Rot_mat = jnp.linalg.inv(Rot_mat)
+    Rot_mat = jnp.linalg.inv(Rot_mat)
     #jdbg.print('{}',Rot_mat)
 
     return Rot_mat
@@ -40,6 +40,19 @@ R_base_to_field = R_base_to_field.at[:,3].set(v_base_to_field)
 def base_to_field(joint_angles):
     return R_base_to_field
 
+x = 0.0
+y = 0.0
+z = -0.4
+M_x = jnp.array([[1,0,0,x], [0,1,0,y], [0,0,1,z], [0,0,0,1]])
+def translation(joint_angles):
+    return M_x
+
+tilt = -jnp.pi/4
+pan = -jnp.pi/2
+M_r = jnp.array([[jnp.cos(pan), -jnp.sin(pan),0,0], [jnp.sin(pan), jnp.cos(pan),0,0], [0,0,1,0], [0,0,0,1]]) @ jnp.array([[jnp.cos(tilt),0,jnp.sin(tilt),0], [0,1,0,0], [-jnp.sin(tilt),0,jnp.cos(tilt),0], [0,0,0,1]])
+def rotation(joint_angles):
+    #jdbg.print('{}', joint_angles/180*jnp.pi)
+    return M_r
 
 # make 3 test transforms, one for arbitrary translations, and one for arbitrary pans, and one for arbitrary tilts. Once they work, i can start to combine them
 #------------------------------------------------------------------------------------
@@ -53,8 +66,10 @@ def get_architecture(args):
     frame_graph.add_edge(source = "base", target = "cam_joint", Transform = Transform({"M_func": base_to_cam_joint}))
     frame_graph.add_edge(source = "cam_base", target = "cam_joint", Transform=Transform({"M_func": cam_base_to_joint}))
     frame_graph.add_edge(source = "base", target="field", Transform=Transform({"M_func": base_to_field}))
+    frame_graph.add_edge(source = "r1", target="r2", Transform=Transform({"M_func": translation}))
+    frame_graph.add_edge(source = "rot1", target="rot2", Transform=Transform({"M_func": rotation}))
 
-    #coord_transf1 = CoordinateTransformation("coord_transf1", {"FrameGraph": frame_graph, "source_frame": "cam_base", "target_frame": "cam_joint"})
+    coord_transf1 = CoordinateTransformation("coord_transf1", {"FrameGraph": frame_graph, "source_frame": "cam_base", "target_frame": "cam_joint"})
 
 
     img_proj = PinHoleBackProjector("img_proj", {"img_shape":(299,448), "focal_length": 0.01, "frustrum_angles": (90,60)})
@@ -64,7 +79,7 @@ def get_architecture(args):
 
 
     tcp_reader = TCPSocket("tcp_reader", {"mode": "read", "ip": "127.0.0.1", "port": 50025, "shape": (299,448), 'time_step': 0.02})
-    #tcp_reader_joints = TCPSocket("tcp_reader_joints", {"mode": "read", "ip": "127.0.0.1", "port": 50022, "shape": (2,1), 'time_step': 0.02})
+    tcp_reader_joints = TCPSocket("tcp_reader_joints", {"mode": "read", "ip": "127.0.0.1", "port": 50022, "shape": (2,1), 'time_step': 0.02})
 
     tcp_lidar_writer = TCPSocket("tcp_lidar_writer", {"mode": "write", "ip": "127.0.0.1", "port": 50001, "shape": (299,448), 'time_step': 0.02})
     tcp_head_writer = TCPSocket("tcp_head_writer", {"mode": "write", "ip": "127.0.0.1", "port": 50002, "shape": (180,180), 'time_step': 0.02})
@@ -80,7 +95,9 @@ def get_architecture(args):
     tcp_reader >> tcp_lidar_writer >> nf2
     tcp_reader >> img_proj >> rim1 >> tcp_head_writer >> nf3
 
-    rim1 >> rim2 >> rim3 >> tcp_allo_writer >> nf1
-    #tcp_reader_joints >> "coord_transf1.in1" 
+    img_proj >> coord_transf1 >> rim3 >> tcp_allo_writer >> nf1
+
+    #rim1 >> rim2 >> rim3 >> tcp_allo_writer >> nf1
+    tcp_reader_joints >> "coord_transf1.in1" 
     
 
