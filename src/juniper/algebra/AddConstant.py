269ed83600991e94f2275657ae5a6b207f4d27db
sehrispk <stephan.sehring@gmail.com>
2026-04-14 15:03:12 +0200

    AddConstant bugfix

--- src/juniper/algebra/AddConstant.py
+++ src/juniper/algebra/AddConstant.py
@@ -31,4 +31,4 @@ class AddConstant(Step):
     def __init__(self, name : str, params : dict):
         mandatory_params = ["constant"]
         super().__init__(name, params, mandatory_params)
-        compute_kernel = compute_kernel_factory(self._params)
+        self.compute_kernel = compute_kernel_factory(self._params)
